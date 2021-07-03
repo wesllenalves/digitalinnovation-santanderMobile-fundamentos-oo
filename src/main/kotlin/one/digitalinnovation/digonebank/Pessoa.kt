ad07126da96eda42ec61572682cda86f6d420c24
wesllenalves <wesllenalves@gmail.com>
2021-07-03 12:59:25 -0300

    Adicionando Classe pessoa

---
+++ src/main/kotlin/one/digitalinnovation/digonebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digonebank
+
+class Pessoa {
+    var nome: String = "Wesllen"
+    var cpf: String = "03230944143"
+}
+
+fun main() {
+    val Clinte  = Pessoa ()
+    println(Clinte.nome)
+    println(Clinte.cpf)
+}
