63b2e027315fcb392b518c673ab09c57dd24f5f0
wesllenalves <wesllenalves@gmail.com>
2021-07-03 13:13:56 -0300

    Adicionando proteção na alteração de dados

--- src/main/kotlin/one/digitalinnovation/digonebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digonebank/Pessoa.kt
@@ -3,10 +3,13 @@ package one.digitalinnovation.digonebank
 class Pessoa {
     var nome: String = "Wesllen"
     var cpf: String = "03230944143"
+    private set
+
 }
 
 fun main() {
     val Clinte  = Pessoa ()
     println(Clinte.nome)
     println(Clinte.cpf)
+
 }
